e7b150ddb93b760c7801dfcd14127a4ead755310
enayahk <enayahk@nycstudents.net>
2025-11-20 08:44:37 -0500

    Add files via upload

--- src/CardGame.java
+++ src/CardGame.java
@@ -7,7 +7,6 @@ public class CardGame {
     private final List<Integer> allCards = new ArrayList<Integer>();
 
 
-
     private List<Integer> userSet = new ArrayList<>(6);
 
     private List<Integer> botSet = new ArrayList<>(6);
@@ -23,7 +22,8 @@ public class CardGame {
     public List<Integer> getUserSet() {
         return userSet;
     }
-// creates initial cardsets, for now length 6 -- might change mind
+
+    // creates initial cardsets, for now length 6 -- might change mind
     public void initializeSets() {
 //        for (int i = 6; i > 0; i--) {
 //            int indexer = 0;
@@ -36,12 +36,11 @@ public class CardGame {
         int randomCard = 0;
 
         for (int i = 0; i < 12; i++) {
-            randomCard = (int)(Math.random()*allCards.size());
+            randomCard = (int) (Math.random() * allCards.size());
             if (i % 2 == 0) {
 
                 userSet.add(allCards.get(randomCard));
-            }
-            else {
+            } else {
 //                System.out.println("Player two gets: " + allCards.get(randomCard));
                 botSet.add(allCards.get(randomCard));
 //                System.out.println("botset:" +botSet);
@@ -49,24 +48,20 @@ public class CardGame {
             allCards.remove(randomCard);
 
 
-
-
         }
         tableCard = Integer.valueOf(allCards.get(randomCard));
-        System.out.println("table card:" +tableCard);
+        System.out.println("The challenger card appears! Its value is: " + tableCard);
     }
     // will be called every time the user's turn arrives
 
     public void printUserSet() {
+        System.out.println(" ");
         System.out.println("Your card set is:" + userSet);
-        if (winner <= 0 ) {
+        if (winner <= 0) {
             System.out.println("You can't see your opponent's cards yet, though!");
         } else {
-            System.out.println("Your card set is: " +userSet+", and your opponent's set is: " +botSet);
+            System.out.println("Your card set is: " + userSet + ", and your opponent's set is: " + botSet);
         }
-
-
-
 
 
     }
@@ -74,8 +69,7 @@ public class CardGame {
 
     public void playerDraws(int draw) {
 
-        if (userSet.contains(draw))
-        {
+        if (userSet.contains(draw)) {
             validChoice = true;
             userChoice = draw;
             System.out.println("Card drawn!");
@@ -91,18 +85,28 @@ public class CardGame {
     }
 
 
-    public void botDraws () {
-        int randomChoice = (int) (Math.random()*botSet.size());
+    public void botDraws() {
+        int randomChoice = (int) (Math.random() * botSet.size());
         botChoice = botSet.get(randomChoice);
         botSet.remove(Integer.valueOf(botChoice));
 //        System.out.println(botChoice);
+        System.out.println("Your opponent draws a card!");
+        System.out.println("Its value is: " +botChoice);
 
     }
 
-//    public boolean gameTime() {
-//        if (Math.abs())
-//
-//        return winner;
-//    }
+    public int gameTime() {
+
+        if (Math.abs(20 - botChoice) > Math.abs(20 - userChoice)) {
+            winner = -1;
+        } else if (Math.abs(20 - botChoice) < Math.abs(20 - userChoice)) {
+            winner = 1;
+        } else {
+            winner = 0;
+        }
+
+
+        return winner;
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         CardGame c = new CardGame();
+        System.out.println("Card game rules : You will be dealt a set of 6 cards.");
+        System.out.println("Match one of them to the table card so that the value is as close to 20 as possible.");
+        System.out.println("You cannot see your opponent's deck.");
+        System.out.println("The closest to 20 wins one point. If you run out of cards or your opponent reaches 3 points before you, you lose.");
+        System.out.println("--------------");
+        System.out.println("There exists a nightmare difficulty if you dare... Will you take the chance? y/n: ");
+        if ((scan.nextLine()).contains("y")) {
+            System.out.println("Good luck..");
+        } else if ((scan.nextLine()).contains("n")) {
+            System.out.println("A wise decision.");
+        }
+
+
         c.initializeSets();
         c.printUserSet();
-        System.out.println("");
+        System.out.println("--------------");
         System.out.println("What card would you like to draw from your deck?");
         int draw = scan.nextInt();
         c.playerDraws(draw);
+        System.out.println("--------------");
         c.botDraws();
-        c.printUserSet();
+//        c.printUserSet();
+        System.out.println("");
+
+
     }
 }
